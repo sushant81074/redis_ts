c09e32417c25e7175f34dbbbfb0f00b8e61f7b44
sushantEz <sushant@ezulix.com>
2026-03-09 16:49:42 +0530

    ye

--- app/main.ts
+++ app/main.ts
@@ -10,10 +10,9 @@ console.log("Logs from your program will appear here!");
 const server: net.Server = net.createServer((socketConn: net.Socket) => {
 
     socketConn.on("data", (d: Buffer) => {
-        console.log(d, d.toString("utf-8"));
         let data = d.toString("utf-8");
-        data.split("\n").forEach(e => socketConn.write("+PONG\r\n"));
-        // socketConn.write("+PONG\r\n");
+        console.log(data);
+        socketConn.write("+PONG\r\n");
     });
 
     socketConn.on("close", (e: boolean) => console.log("connection closed ?", e));
